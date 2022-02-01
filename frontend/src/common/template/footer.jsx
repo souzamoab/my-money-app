13556357d6587a4aaddfe030a804e4c905082603
Fabricio Lima <fabricio1099@gmail.com>
2022-02-01 19:08:17 -0300

    Up frontend

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      <a href="https://github.com/souzamoab/my-money-app" target="_blank">
+        M & F
+      </a>
+      .
+    </strong>
+  </footer>
+);
